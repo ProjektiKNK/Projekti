9d12a968383ae772a84b05080195e69ab2937386
JetliraFetahaj <59027249+JetliraFetahaj@users.noreply.github.com>
2020-06-13 07:21:49 -0700

    Add files via upload

---
+++ src/application/Controllers/SemestriController.java
@@ -0,0 +1,36 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class SemestriController {
+
+    @FXML
+    private Label registerStudents;
+
+    @FXML
+    private Label searchStudents;
+
+    @FXML
+    private Label reportStudents;
+
+    @FXML
+    private Label semesterStudents;
+
+    @FXML
+    private Label registerSemester;
+
+    @FXML
+    private ImageView contact;
+
+    @FXML
+    private ImageView settingsHelp;
+
+    @FXML
+    private ImageView sendMessage;
+
+    @FXML
+    private Label editmyProfile;
+
+}
