36c5942b1f3020fa0082a4217a1df5c41b0e5567
JetliraFetahaj <59027249+JetliraFetahaj@users.noreply.github.com>
2020-06-13 07:23:25 -0700

    Add files via upload

--- src/application/Controllers/Main.java
+++ src/application/Controllers/Main.java
@@ -1,38 +1,21 @@
-package application.Controllers;
+package application;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-
-
-import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		
-		Parent parent = FXMLLoader.load(getClass().getResource("../Fxmls/MainXML.fxml"));
-		//Pane parent=new Pane();
-		
-		Scene scene=new Scene(parent);
-		
+		Parent root=FXMLLoader.load(getClass().getResource("fx.fxml"));
+		Scene scene= new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
 	}
-
-
-	 public static void main(String[] args) {
-	 Application.launch(args);
-	 }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
 
-
-	
