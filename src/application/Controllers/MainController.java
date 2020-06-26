1fd9c866cb8464f1558df81b72a9e4f5f5a67e15
mergograiqevci <59068563+mergograiqevci@users.noreply.github.com>
2020-06-26 11:51:25 +0200

    Add files via upload

--- src/application/Controllers/MainController.java
+++ src/application/Controllers/MainController.java
@@ -64,14 +64,43 @@ public class MainController {
     
     
     @FXML
-    void onMouseClickedMainLeft(MouseEvent event) throws IOException {
+    void fxmlChanges(MouseEvent event) throws IOException {
     	if(event.getSource()==editmyProfile1)
     	{
     		AnchorPane pane=FXMLLoader.load(getClass().getResource("../Fxmls/MyProfile.fxml"));
     		anchorMain.getChildren().setAll(pane);
     	}
-    	
-
+    	else if(event.getSource()==registerSemester1 || event.getSource()==registerSemesterMain1)
+    	{
+    		AnchorPane pane=FXMLLoader.load(getClass().getResource("../Fxmls/fx.fxml"));
+    		anchorMain.getChildren().setAll(pane);
+    	}
+    	else if(event.getSource()==reportStudents1 || event.getSource()==ReportStudentsMain1)
+    	{
+    		AnchorPane pane=FXMLLoader.load(getClass().getResource("../Fxmls/raport.fxml"));
+    		anchorMain.getChildren().setAll(pane);
+    	}
+    	else if(event.getSource()==searchStudents1 || event.getSource()==searchStudentsMain1)
+    	{
+    		AnchorPane pane=FXMLLoader.load(getClass().getResource("../Fxmls/EditDeleteUser.fxml"));
+    		anchorMain.getChildren().setAll(pane);
+    	}
+    	else if(event.getSource()==registerStudentsmain1 || event.getSource()==registerStudents1)
+    	{
+    		AnchorPane pane=FXMLLoader.load(getClass().getResource("../Fxmls/studentregister.fxml"));
+    		anchorMain.getChildren().setAll(pane);
+    	}
+    	else if(event.getSource()==sendMessageMain1 || event.getSource()==sendMessage1)
+    	{
+    		AnchorPane pane=FXMLLoader.load(getClass().getResource("../Fxmls/FXMLperSend.fxml"));
+    		anchorMain.getChildren().setAll(pane);
+    	}
+    	else if(event.getSource()==contact1 || event.getSource()==contactMain1)
+    	{
+    		AnchorPane pane=FXMLLoader.load(getClass().getResource("../Fxmls/ContactUs.fxml"));
+    		anchorMain.getChildren().setAll(pane);
+    	}
+	
     }
     
 
