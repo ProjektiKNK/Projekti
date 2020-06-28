0b2c3f6a61526610b32c294bc19bf6e745cc345d
Linda-Imeri <58806142+Linda-Imeri@users.noreply.github.com>
2020-06-28 18:50:59 +0200

    Add files via upload
    Updated WriteMessageControl class

--- src/application/Controllers/WriteMessageControl.java
+++ src/application/Controllers/WriteMessageControl.java
@@ -1,47 +1,103 @@
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.text.StyledEditorKit.BoldAction;
+
+import com.itextpdf.text.Font.FontStyle;
+import com.sun.glass.ui.Size;
+import com.sun.mail.imap.Rights.Right;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
-public class WriteMessageControl implements Initializable{
+public class WriteMessageControl implements Initializable {
 	@FXML
-    private ColorPicker colorpicker;
+	private ColorPicker colorpicker;
+	@FXML
+	private Slider TextSizeSlider;
+	@FXML
+	private Text mytext;
+	@FXML
+	private TextArea textarea;
 
-    @FXML
-    private Text mytext;
-    @FXML
-    private Label label;
-    @FXML
-    private TextArea textarea;
-    @FXML
-    private Button dergo;
-    
-    @FXML
-    void changecolor(ActionEvent event) {
-    	Color c=colorpicker.getValue();
-    	mytext.setFill(c);
-    	
-    	int color=colorpicker.getValue().hashCode();
-    	String c2=Integer.toHexString(color);
-    	
-    	textarea.setStyle("-fx-background-color: #"+c2);
-    	dergo.setStyle("-fx-background-color: #"+c2);
-    }
-    @FXML
-    void dergo(ActionEvent event) {
-    	//nese shtypet butoni dergo
-    }
+	@FXML
+	private Button dergo;
+
+	@FXML
+	void changecolor(ActionEvent event) {
+		Color c = colorpicker.getValue();
+		mytext.setFill(c);
+
+		int color = colorpicker.getValue().hashCode();
+		String c2 = Integer.toHexString(color);
+
+		textarea.setStyle("-fx-background-color: #" + c2);
+		dergo.setStyle("-fx-background-color: #" + c2);
+
+	}
+
+	@FXML
+	void dergo(ActionEvent event) {
+		try{Mailer.send("sems.noreply@gmail.com", "spms12345", "linndaimmeri@gmail.com", "", textarea.getText());
+		textarea.setText("");}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+
+	}
+
+	@FXML
+	void SetCenter(MouseEvent event) {
+		mytext.setTextAlignment(TextAlignment.CENTER);
+	}
+
+	@FXML
+	void BoldText(MouseEvent event) {
+		mytext.setStyle("-fx-font-style:" + FontWeight.BOLD);
+	}
+
+	@FXML
+	void SetLeft(MouseEvent event) {
+		mytext.setTextAlignment(TextAlignment.LEFT);
+	}
+
+	@FXML
+	void SetRight(MouseEvent event) {
+		mytext.setTextAlignment(TextAlignment.RIGHT);
+	}
+
+	@FXML
+	void italicText(MouseEvent event) {
+		mytext.setStyle("-fx-font-style:" + FontStyle.ITALIC);
+	}
+
+	@FXML
+	void underline(MouseEvent event) {
+		mytext.setStyle("-fx-font-style:" + FontStyle.UNDERLINE);
+	}
+
+	@FXML
+	void ChangeTextSize(MouseEvent event) {
+		double vlera = TextSizeSlider.getValue();
+		textarea.setStyle("-fx-font-size:" + vlera);
+
 	}
 
 }
