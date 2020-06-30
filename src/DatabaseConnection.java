db658b2dff1a35995bd52b40f034fed797781ce8
Linda-Imeri <58806142+Linda-Imeri@users.noreply.github.com>
2020-06-30 11:35:50 +0200

    Add files via upload
    Update DatabaseConnection Class

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,25 +1,28 @@
-package application;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class DatabaseConnection {
-	
-	public static Connection startConnection() {		
+public  Connection startConnection() {		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/sm?characterEncoding=UTF-8&useSSL=false", "root", "");
+			String dbName="spms";
+			String userName="root";
+			String password="";
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/"+dbName,userName,password);
+			
 			return conn;
 		} 
 		catch (SQLException e) {
-			System.out.println("Error");
+			System.out.println("Error"+e);
 			return null;
 		}
 		catch (ClassNotFoundException e) {	
-			System.out.println("Error");			
+			System.out.println("Error"+e);			
 			return null;
 		} 
 	}
 }
+
+
